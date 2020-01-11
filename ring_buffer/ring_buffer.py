5523175bf9cd161d55d5f06d95e61e17fab38ed0
LD <LDinho@users.noreply.github.com>
2020-01-11 11:28:37 -0800

    add ring buffer soln

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,31 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.capacity == 0:  # don't append if capacity = zero
+            return
+
+        if len(self.storage) == self.capacity:
+            if not self.current:
+                self.current = self.storage.tail  # replace when capacity is full
+
+            self.current.value = item
+            self.current = self.current.prev
+        else:
+            self.storage.add_to_head(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        if len(self.storage) == 0:
+            return list_buffer_contents  # return empty list if storage is empty
+
+        current = self.storage.tail
+        list_buffer_contents.append(current.value)
+
+        while current.prev:
+            current = current.prev
+            list_buffer_contents.append(current.value)
 
         return list_buffer_contents
 
